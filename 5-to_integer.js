3961e49000a2d9997ff7b64f6099b3de56cc1af9
Musabe Yvan <yvanzomusa@gmail.com>
2025-07-09 09:32:54 +0200

    Correct appeared issue

--- 5-to_integer.js
+++ 5-to_integer.js
@@ -1,3 +1,7 @@
 const args = process.argv.slice(2);
-
-console.log("My number: " + Number(args[0]));
+if(args[0] == Number){
+  console.log("My number: " + Number(args[0]));
+  
+} else {
+  console.log("Not a number")
+}
