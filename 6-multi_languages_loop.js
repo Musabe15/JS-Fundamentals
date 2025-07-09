0f4405516967ae979cdece3086187361bc484c48
Musabe Yvan <yvanzomusa@gmail.com>
2025-07-09 09:52:20 +0200

    Added a script which use while loop to iterate and generate output

---
+++ 6-multi_languages_loop.js
@@ -0,0 +1,7 @@
+let langLoop = ["C is fun", "Python is cool", "JavaScript is amazing"];
+let i = 0;  // index initializing
+
+while(i < langLoop.length){   // stoping condition
+  console.log(langLoop[i]);   // printing the output
+  i++;                        // incrementing by one per each iteration
+}
