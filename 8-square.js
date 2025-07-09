de647e8bab62d058903069a8aba2d7f5bf1b4a4a
Musabe Yvan <yvanzomusa@gmail.com>
2025-07-09 16:32:40 +0200

    Added a script that print a square

---
+++ 8-square.js
@@ -0,0 +1,13 @@
+const x = parseInt(process.argv[2]);
+
+if(isNaN(x)){
+  console.log("Missing size");
+} else {
+  for(let i = 0; i < x; i++){
+    let hor ="";
+    for(let j = 0; j < x; j++){
+      hor += "x";
+    }
+    console.log(hor);
+  }
+}
