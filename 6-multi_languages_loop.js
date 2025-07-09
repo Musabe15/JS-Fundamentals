b7e6174c6f6322403c89ecaa712127aee9062aa8
Musabe Yvan <yvanzomusa@gmail.com>
2025-07-09 18:17:04 +0200

    Corrected issue

--- 6-multi_languages_loop.js
+++ 6-multi_languages_loop.js
@@ -1,7 +1,7 @@
-let langLoop = ["C is fun", "Python is cool", "JavaScript is amazing"];
-let i = 0;  // index initializing
+const lines = [
+  "C is fun",
+  "Python is cool",
+  "JavaScript is amazing"
+];
 
-while(i < langLoop.length){   // stoping condition
-  console.log(langLoop[i]);   // printing the output
-  i++;                        // incrementing by one per each iteration
-}
+lines.forEach(line => console.log(line));
