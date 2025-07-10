30d920ea761879ef7d70c553f3ed0d9bfa85b986
Musabe Yvan <yvanzomusa@gmail.com>
2025-07-10 10:17:56 +0200

    fith correction

--- 8-square.js
+++ 8-square.js
@@ -3,9 +3,9 @@ const x = parseInt(process.argv[2]);
 if(isNaN(x)){
   console.log("Missing size");
 } else {
-  for(let i = 0; i < x; i++){
+  for (let i = 0; i < x; i++){
     let hor ="";
-    for(let j = 0; j < x; j++){
+    for (let j = 0; j < x; j++){
       hor += "X";
     }
     console.log(hor);
