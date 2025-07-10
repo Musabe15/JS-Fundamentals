d2021f34f60926c36f9d38cc790953a07ead2908
Musabe Yvan <yvanzomusa@gmail.com>
2025-07-10 09:17:53 +0200

    Second correction

--- 7-multi_c.js
+++ 7-multi_c.js
@@ -1,13 +1,9 @@
 const x = parseInt(process.argv[2]);
 
-if(isNaN(x) || x < 0){
-  console.log("Missing number of occurences");
+if (isNaN(x) || x < 0) {
+  console.log("Missing number of occurrences");
 } else {
-  let i = 0;
-  while(i < x){
+  for (let i = 0; i < x; i++) {
     console.log("C is fun");
-    i++;
   }
 }
-
-
